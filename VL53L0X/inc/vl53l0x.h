85eb13eebd44f11dde5c58ccac95461fcf7bcf11
Daniel Nery <danielnso97@gmail.com>
2019-05-10 19:52:35 -0300

    Add config structs
    - Split LSM header in two

--- VL53L0X/inc/vl53l0x.h
+++ VL53L0X/inc/vl53l0x.h
@@ -16,6 +16,7 @@
 #include <stdint.h>
 
 #include "i2c.h"
+#include "gpio.h"
 
 /*****************************************
  * Public Constants
@@ -25,15 +26,19 @@
 #define VL53L0X_TIMEOUT_RETURN_VALUE (65535) /**< Value returned if getRange times out */
 
 /*****************************************
- * Public Function Prototypes
+ * Public Types
  *****************************************/
 
-/**
- * @brief Sets I2C handler for VL53L0X devices.
- *
- * @note This must be called first.
- */
-void vl53l0x_i2c_set(I2C_HandleTypeDef* hi2c);
+typedef struct __attribute__((packed)) vl53l0x {
+    uint8_t addr;
+    I2C_HandleTypeDef* hi2c;
+    GPIO_TypeDef* xshut_port;
+    uint16_t xshut_pin;
+} vl53l0x_t;
+
+/*****************************************
+ * Public Function Prototypes
+ *****************************************/
 
 /**
  * @brief Initializes a single VL53L0X device.
@@ -44,44 +49,38 @@ void vl53l0x_i2c_set(I2C_HandleTypeDef* hi2c);
  *
  * @return 0 on failure, 1 on success.
  */
-uint8_t vl53l0x_init();
+uint8_t vl53l0x_init(vl53l0x_t* vl53l0x);
+
+void vl53l0x_xshut_off(vl53l0x_t* vl53l0x);
+
+void vl53l0x_xshut_on(vl53l0x_t* vl53l0x);
 
 /**
  * @brief Changes current device address.
  *
  * @param new_addr 8 bit address to be set
  */
-void vl53l0x_set_dev_address(uint8_t new_addr);
-
-/**
- * @brief Change the current working address.
- *
- * @note Subsequent functions will be applied to the sensor
- *       with the address selected here.
- *
- * @param new_addr 8 bit address to be set
- */
-void vl53l0x_set_current_address(uint8_t new_addr);
+void vl53l0x_set_dev_address(vl53l0x_t* vl53l0x, uint8_t new_addr);
 
 /**
  * @brief Start current device continuous measuring.
  *
  * @param period_ms Measuring period
  */
-void vl53l0x_start_continuous(uint32_t period_ms);
+void vl53l0x_start_continuous(vl53l0x_t* vl53l0x, uint32_t period_ms);
 
 /**
  * @brief Stop current device continuous measuring.
  */
-void vl53l0x_stop_continuous();
+void vl53l0x_stop_continuous(vl53l0x_t* vl53l0x);
 
 /**
  * @brief Get the current device's last measured range.
  *
  * @note vl53l0x_start_continuous must be called first.
  *
- * @return Range in millimeters or 65535 on failure.
+ * @return Range in millimeters or VL53L0X_TIMEOUT_RETURN_VALUE on failure.
  */
-uint16_t vl53l0x_get_range();
+uint16_t vl53l0x_get_range(vl53l0x_t* vl53l0x);
 
 #endif  // __VL53L0X_H__
